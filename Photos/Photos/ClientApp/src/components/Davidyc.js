61940af3f0ae27eee88613e7de896a3cc66f56f4
Sergey Davydov <Sergey_Davydov2@EPKZKARW0882>
2021-06-01 22:36:47 +0600

    Day 1. Add new components

---
+++ Photos/Photos/ClientApp/src/components/Davidyc.js
@@ -0,0 +1,12 @@
+﻿import React, { Component } from 'react';
+
+export class Davidyc extends Component {
+ 
+    render() {
+        return (
+            <div>
+                <h1>Davidyc</h1>              
+            </div>
+        );
+    }
+}
