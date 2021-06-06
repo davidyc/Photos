4d732242150de64680641fc1dd138c7e7c5cf3d6
Sergey Davydov <Sergey_Davydov2@EPKZKARW0882>
2021-06-06 12:16:31 +0600

    Day 2  add extenstion sql

---
+++ Photos/Photos.Application/Extensions/DependencyInjection/DbContextServiceCollectionExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Photos.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Photos.Application.Extensions.DependencyInjection
+{
+    public static class DbContextServiceCollectionExtensions
+    {
+        public static IServiceCollection AddPhotosDbContext(this IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddDbContext<PhotosDBContext>((services, options) =>
+            {
+                var configuration = services.GetRequiredService<IConfiguration>();
+                options.UseSqlServer(configuration.GetConnectionString("PhotosDBContext"));
+            });
+
+            return serviceCollection;
+        }
+    }
+}
--- Photos/Photos/Startup.cs
+++ Photos/Photos/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Photos.Application.Extensions.DependencyInjection;
 using Photos.Infrastructure;
 
 namespace Photos
@@ -30,8 +31,9 @@ namespace Photos
             {
                 configuration.RootPath = "ClientApp/build";
             });
-            services.AddDbContext<PhotosDBContext>(options =>
-                       options.UseSqlServer(Configuration.GetConnectionString("PhotosDBContext")));
+
+            services.AddPhotosDbContext();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
