033cb27925ecd3b418153804ec12f7ee4a82782f
Sergey Davydov <Sergey_Davydov2@EPKZKARW0882>
2021-06-15 18:06:39 +0600

    day 5 add qrcode generation button

--- Photos/Photos/ClientApp/src/components/QRCode.js
+++ Photos/Photos/ClientApp/src/components/QRCode.js
@@ -4,31 +4,60 @@ export class QRCode extends Component {
 
     constructor(props) {
         super(props);
-        this.state = { QR: null, loading: true };
+        this.state = { QR: '', loading: false, Text: 'Text' };
+
+        this.handleChange = this.handleChange.bind(this);
     }
 
-    componentDidMount() {
-        this.testData("XXXXX")
+    handleChange(event) {
+        this.setState({value: event.target.value});
     }
 
-    //var base64Image;
 
     render() {
-
-        let contents = this.state.loading
-            ? <p><em>Loading...</em></p>
-            : <img src={"data:image/png;base64," + this.state.QR.qrCode} alt={"data:image/png;base64," + this.state.QR.qrCode} />
-
         return (
-            <div>             
-                <p>{contents}</p>
+            <div>    
+                <div className="input-group">
+                    <div className="input-group-prepend">
+                        <span className="input-group-text" id="basic-addon">
+                            <i className="fas fa-pencil-alt prefix"></i>
+                        </span>
+                    </div>
+                    <textarea className="form-control" id="exampleFormControlTextarea1" 
+                    value={this.state.Text} onChange={this.handleChange}
+                    rows="5" />
+                </div>
+
+                <div style={{ marginTop: "60px" }}>
+                    <button className="button" onClick={this.fetchData} disabled={this.state.loading}>
+                        {this.state.loading && (
+                            <i
+                                className="fa fa-refresh fa-spin"
+                                style={{ marginRight: "5px" }}
+                            />
+                        )}
+                        {this.state.loading && <span>QR Code is generating</span>}
+                        {!this.state.loading && <span>Generate QR Code</span>}
+                    </button>
+                </div>
+                <img src={"data:image/png;base64," + this.state.QR.qrCode} />
             </div>
         );
     }
 
+    fetchData = () => {
+        this.setState({ loading: true });
+        this.testData(this.state.Text)
+        //Faking API call here
+        setTimeout(() => {
+            this.setState({ loading: false });
+        }, 2000);
+    };
+
     async testData(text) {
         const response = await fetch('QRCode/?text=' + text);
         const data = await response.json();
-        this.setState({ QR: data, loading: false });
+        this.setState({ QR: data});
     }
+
 }
